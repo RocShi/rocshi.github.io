f02711a69bcc05281547f31b0e7ca6f90f9eb179
ShiPeng <RocShi@outlook.com>
2021-04-22 22:54:55 +0800

    Site updated: 2021-04-22 22:54:49

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -16,10 +16,10 @@
      try {
          TagCanvas.textFont = 'Trebuchet MS, Helvetica';
          TagCanvas.textColour = '#333';
-         TagCanvas.textHeight = 25;
+         TagCanvas.textHeight = 15;
          TagCanvas.outlineColour = '#E2E1D1';
-         TagCanvas.maxSpeed = 0.5;
-         TagCanvas.freezeActive = false;
+         TagCanvas.maxSpeed = 0.1;
+         TagCanvas.freezeActive = true;
          TagCanvas.outlineMethod = 'block';
          TagCanvas.minBrightness = 0.2;
          TagCanvas.depth = 0.92;
